0f54b61e3fd36c5cdb9d3b465c10cd435b268966
Lakshmi <51483670+emandivaralakshmi@users.noreply.github.com>
2019-07-27 21:01:45 +0530

    Installed packages redux, react-redux, redux-thunk and integrated Login API and implemented error validations for userName and password.

---
+++ src/components/Common/Input/Input.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { FormGroup, Label, Input, FormFeedback } from "reactstrap";
+import classes from './Input.module.css';
+const CustInput = (props) => {
+    let inputElement = null;
+    const inputClasses = [classes.InputElement];
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
+    switch (props.elementType) {
+        case ('input'):
+            inputElement =
+                <FormGroup>
+                    <Label className={classes.Label}>{props.label}</Label>
+                    <Input
+                        className={inputClasses.join(' ')}
+                        {...props.elementConfig}
+                        value={props.value}
+                        invalid={props.invalid && props.touched}
+                        onChange={props.changed} />
+                    <FormFeedback>{props.errorMessage}</FormFeedback>
+                </FormGroup>;
+            break;
+        case ('textarea'):
+            inputElement = <textarea
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={inputClasses.join(' ')}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
+        case ('checkbox'):
+            inputElement = (
+                <FormGroup check>
+                    <Label checked={props.elementConfig.checked} className={classes.Label}>
+                        <Input
+                            className={inputClasses.join(' ')}
+                            {...props.elementConfig}
+                            value={props.value}
+                            onChange={props.changed} />
+                        {props.elementConfig.label}
+                    </Label>
+                </FormGroup>
+            );
+            break;
+        default:
+            inputElement =
+                <FormGroup>
+                    <Label className={classes.Label}>{props.label}</Label>
+                    <Input
+                        className={inputClasses.join(' ')}
+                        {...props.elementConfig}
+                        value={props.value}
+                        invalid={props.invalid && props.touched}
+                        onChange={props.changed} />
+                    <FormFeedback>Please write at least 3 characters.</FormFeedback>
+                </FormGroup>;
+    }
+    return (
+        <div className={classes.Input}>
+            {inputElement}
+        </div>
+    );
+};
+export default CustInput;
+
