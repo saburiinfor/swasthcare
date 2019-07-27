3f91d07dcc436ab993186cae7b116c839146fbde
Lakshmi <51483670+emandivaralakshmi@users.noreply.github.com>
2019-07-27 21:00:02 +0530

    Installed packages redux, react-redux, redux-thunk and integrated Login API and implemented error validations for userName and password.

---
+++ src/store/actions/auth.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import FormData from 'form-data';
+import * as actionTypes from './actionTypes';
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
+    };
+};
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = new FormData();
+        authData.append("email", email);
+        authData.append("password", password);
+        axios.post("https://www.skrepository.com/restapi/User/login/", authData).then(
+            response => {
+                console.log("res ***" + JSON.stringify(response));
+                if (response.data.success === 1) {
+                    localStorage.setItem('token', response.data.token);
+                    dispatch(authSuccess(response.data.token, response.data.id));
+                }
+                else {
+                    dispatch(authFail(response.data.error.errormsg));
+                }
+            })
+            .catch(err => {
+                dispatch(authFail(err.message));
+            });
+    };
+};
+export const setAuthRedirectPath = (path) => {
+    return {
+        type: actionTypes.SET_AUTH_REDIRECT_PATH,
+        path: path
+    };
+};
