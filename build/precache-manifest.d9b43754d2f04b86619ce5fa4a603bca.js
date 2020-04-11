9398b1b3d7a0474c0ee2a59aa73447a9b6ed0277
Ravi Kiran Lingam <lravikiran@icloud.com>
2020-04-11 14:10:00 +0530

    Resetting the project

---
+++ build/precache-manifest.d9b43754d2f04b86619ce5fa4a603bca.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b8375902fb6105153cd0d3aca4d6e4",
+    "url": "/swasthcare/index.html"
+  },
+  {
+    "revision": "59ac90f549bd134ca73a",
+    "url": "/swasthcare/static/css/2.d6847abf.chunk.css"
+  },
+  {
+    "revision": "624790927dae06de9172",
+    "url": "/swasthcare/static/css/main.9e60887d.chunk.css"
+  },
+  {
+    "revision": "59ac90f549bd134ca73a",
+    "url": "/swasthcare/static/js/2.51038c62.chunk.js"
+  },
+  {
+    "revision": "17036bc74d35c0bf77fc7c63a5cc74f0",
+    "url": "/swasthcare/static/js/2.51038c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624790927dae06de9172",
+    "url": "/swasthcare/static/js/main.a4808b8c.chunk.js"
+  },
+  {
+    "revision": "7b5ce91b944c790249a8",
+    "url": "/swasthcare/static/js/runtime-main.c90d2062.js"
+  },
+  {
+    "revision": "2dc2afa129ab6209887d5766c79f3527",
+    "url": "/swasthcare/static/media/CK1.2dc2afa1.jpg"
+  },
+  {
+    "revision": "9f382b5f772cdb16350353dfa1771ce1",
+    "url": "/swasthcare/static/media/CK2.9f382b5f.jpg"
+  },
+  {
+    "revision": "1e847968bf8732eaf1a3203ca15fe89d",
+    "url": "/swasthcare/static/media/CK3.1e847968.jpg"
+  },
+  {
+    "revision": "fb8a4d06a1f7a694e4ccde91f6f730d8",
+    "url": "/swasthcare/static/media/bannerImg2.fb8a4d06.jpg"
+  },
+  {
+    "revision": "c2bb48acbe4e128b5514273214222e47",
+    "url": "/swasthcare/static/media/bannerImg3.c2bb48ac.jpg"
+  },
+  {
+    "revision": "723494a45f26bb1427c6d43205333ff1",
+    "url": "/swasthcare/static/media/flags.723494a4.png"
+  }
+]);
