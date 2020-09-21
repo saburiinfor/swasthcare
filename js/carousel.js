5440fe90e24c613435b1aa766763c86f2228fd74
Your Name <mahendra.mohanty@yahoo.com>
2020-09-21 18:02:09 +0530

    update conferkare frontpage

---
+++ js/carousel.js
@@ -0,0 +1,210 @@
+$(document).ready(function() {
+    
+	
+	$('#news-slider99').owlCarousel({
+    autoplay:3000,
+    items: 3,
+    loop:true,
+   
+	itemsDesktop:[1199,1],
+        itemsDesktopSmall:[1000,1],
+        itemsMobile:[600,1],
+    singleItem:true,
+    
+	 responsive: {
+          0: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          600: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          1050: {
+            items: 1,
+             autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          }
+        }
+    
+});
+
+    
+    $("#doctor-consult").owlCarousel({
+        
+	autoplay:3000,
+    items: 3,
+    loop:true,
+   
+	itemsDesktop:[1199,3],
+        itemsDesktopSmall:[1000,3],
+        itemsMobile:[600,1],
+    singleItem:true,
+    
+	 responsive: {
+          0: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          600: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          1050: {
+            items: 4,
+             autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          }
+        }
+    
+    });
+	
+	 $("#lab-test").owlCarousel({
+        
+	autoplay:3000,
+    items: 3,
+    loop:true,
+	
+   
+	itemsDesktop:[1199,3],
+        itemsDesktopSmall:[1000,3],
+        itemsMobile:[600,1],
+    singleItem:true,
+    
+	 responsive: {
+          0: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          600: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          1050: {
+            items: 3,
+             autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          }
+        }
+    
+    });
+	 $("#pharmacy").owlCarousel({
+        
+	autoplay:3000,
+    items: 3,
+    loop:true,
+	
+   
+	itemsDesktop:[1199,3],
+        itemsDesktopSmall:[1000,3],
+        itemsMobile:[600,1],
+    singleItem:true,
+    
+	 responsive: {
+          0: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          600: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          1050: {
+            items: 3,
+             autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          }
+        }
+    
+    });
+	$("#health-service").owlCarousel({
+        
+	autoplay:3000,
+    items: 3,
+    loop:true,
+	
+   
+	itemsDesktop:[1199,3],
+        itemsDesktopSmall:[1000,3],
+        itemsMobile:[600,1],
+    singleItem:true,
+    
+	 responsive: {
+          0: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          600: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          1050: {
+            items: 4,
+             autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          }
+        }
+    
+    });
+	$("#radiology").owlCarousel({
+        
+	autoplay:3000,
+    items: 3,
+    loop:true,
+	
+   
+	itemsDesktop:[1199,3],
+        itemsDesktopSmall:[1000,3],
+        itemsMobile:[600,1],
+    singleItem:true,
+    
+	 responsive: {
+          0: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          600: {
+            items: 1,
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          },
+          1050: {
+            items: 4,
+             autoHeight: true,
+            mouseDrag: false,
+            touchDrag: true
+          }
+        }
+    
+    });
+    
+});
+
